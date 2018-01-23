66f4004e8d21580a1cb7984ed49a5230f0a58609
Arvic <arvic10497@gmail.com>
2018-01-23 11:48:39 +0800

    modified run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
+@echo off
 javac -cp "lib/*;C:/Program Files/Java/jre1.8.0_131/lib/*" -d bin src/com/selenium/webdriver/basic/*.java
 
 java -cp bin;lib/*;resources/ org.testng.TestNG config/testng.xml
