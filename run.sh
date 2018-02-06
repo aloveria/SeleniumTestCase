149a33b599473d6a821cf6b638a1027e08ac3ba0
aloveria <32290851+aloveria@users.noreply.github.com>
2018-02-06 15:26:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-CLASSPATH=lib/*:bin:resources/
 javac -cp "lib/*" -d bin src/com/selenium/webdriver/basic/*.java
 
-java -classpath ".:CLASSPATH" org.testng.TestNG config/testng.xml
+java -cp ".:/c/Users/Arvic/Desktop/Selenium/SeleniumDemo/lib/*.jar" org.testng.TestNG config/testng.xml
