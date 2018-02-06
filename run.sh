697bc38d34c13124b6587a88bbd635eac3f6062c
aloveria <32290851+aloveria@users.noreply.github.com>
2018-02-06 15:16:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-javac -cp "lib/* -d bin src/com/selenium/webdriver/basic/*.java
+javac -cp "lib/*" -d bin src/com/selenium/webdriver/basic/*.java
 
 java -cp "bin:lib/*:resources/" org.testng.TestNG config/testng.xml
