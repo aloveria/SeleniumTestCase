6d2b5beb27b4ba189541bbc2ad8c397be03be467
aloveria <32290851+aloveria@users.noreply.github.com>
2018-02-06 15:27:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 javac -cp "lib/*" -d bin src/com/selenium/webdriver/basic/*.java
 
-java -cp ".:/c/Users/Arvic/Desktop/Selenium/SeleniumDemo/lib/*.jar" org.testng.TestNG config/testng.xml
+java -cp "c/Users/Arvic/Desktop/Selenium/SeleniumDemo/lib/*.jar" org.testng.TestNG config/testng.xml
