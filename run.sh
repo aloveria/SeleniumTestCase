de74197df5ca31e5c5808c5ffffa0019e7d51c68
aloveria <32290851+aloveria@users.noreply.github.com>
2018-02-06 14:08:05 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-javac -cp "$JENKINS_HOME/workspace/$JOB_NAME/lib/*:" -d bin src/com/selenium/webdriver/basic/*.java
+javac -cp "$JENKINS_HOME"/workspace/$JOB_NAME/lib/*: -d bin src/com/selenium/webdriver/basic/*.java
 
-java -cp "$JENKINS_HOME/workspace/$JOB_NAME/bin:$JENKINS_HOME/workspace/$JOB_NAME/lib/*:resources/" org.testng.TestNG config/testng.xml
+java -cp "$JENKINS_HOME"/workspace/$JOB_NAME/bin:"$JENKINS_HOME"/workspace/$JOB_NAME/lib/*:resources/ org.testng.TestNG config/testng.xml
